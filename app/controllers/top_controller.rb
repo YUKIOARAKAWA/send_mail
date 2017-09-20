d21b4e9183ac6d46353b9512c43905ef6e9ac17d
荒川幸生 <arakawayukio@arakawasachio-no-MacBook-Air.local>
2017-09-21 08:53:37 +0900

    リンク作成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,9 @@
 class TopController < ApplicationController
   def index
   end
+
+  def send_mail
+    SampleMailer.send_when_update().deliver
+    redirect_to action: :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #get 'top/index'
   root to: "top#index"
+  get 'send_mail', to: "top#send_mail"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
